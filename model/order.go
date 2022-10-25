17fcab1f0dd39357417ab4316579632e1d79f754
Septian Alfa Rizki <57479212+septianar6@users.noreply.github.com>
2022-10-25 09:59:00 +0700

    Delete model directory

--- model/order.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Order struct {
-	Order_ID      int    `gorm:"primaryKey" json:"order_id"`
-	Customer_Name string `gorm:"not null;type:varchar(50)" json:"customer_name"`
-	Ordered_at    string `gorm:"not null;type:varchar(50)" json:"ordered_at"`
-}
